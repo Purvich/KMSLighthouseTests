6fa988fcbbeeeeaa1007421cca24c37bf604cd5f
purvich <purvich90@gmail.com>
2022-03-18 14:05:58 +0300

    update files

--- src/test/java/FirstScenarioTest.java
+++ src/test/java/FirstScenarioTest.java
@@ -1,11 +1,14 @@
 import kmsLighthouse.pages.MainPage;
 import kmsLighthouse.Params;
 import kmsLighthouse.pages.WelcomePage;
+import kmsLighthouse.pages.YouTubeVideoPage;
 import org.junit.jupiter.api.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class FirstScenarioTest extends BaseTest {
 
     @Test
+    @Order(1)
     void firstScenario() {
 
         /*
@@ -27,5 +30,23 @@ public class FirstScenarioTest extends BaseTest {
                 .clickButtonSubmit();
 
         Assertions.assertEquals(Params.SUCCESS_TEXT, new WelcomePage(driver).successText());
+    }
+
+    @Test
+    @Order(2)
+    void secondScenario() {
+
+        /*
+         * Open the following page: https://www.kmslh.com/automation-test/
+         * on the redirected page, wait for the popup and click copy link button
+         * open the copied link.
+         * Verify GE Healthcare: A KMS Lighthouse Success Story video is opened.
+         */
+
+        new MainPage(driver)
+                .waitForPopup()
+                .goToVideoPage();
+
+        Assertions.assertEquals(Params.NAME_VIDEO, new YouTubeVideoPage(driver).successText());
     }
 }
